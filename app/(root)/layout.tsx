e9f9597801e0956ab976c492155dda0d6f824a65
KIMLENDING <gotslaakaak@gmail.com>
2024-11-24 23:43:45 +0900

    rightSidebar제거

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
                         </div>
                     </div>
                 </section>
-                <RightSidebar />
+                {/* <RightSidebar /> */}
             </main>
         </div>
     );
